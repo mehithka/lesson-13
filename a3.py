e25db0b0bc9bc03e0a6d5e882e07edced32d2c7a
mehithka <mehith.karacasia@gmail.com>
2025-01-09 15:32:38 +0500

    a3

---
+++ a3.py
@@ -0,0 +1,28 @@
+def add(P,Q):
+  added =  P+Q 
+  print(added)
+def subtract(P,Q):
+    subtracted = P-Q
+    print(subtracted)
+def multiply(P,Q):
+    multiplied = P*Q
+    print(multiplied)
+def divide(P,Q):
+    divided = P/Q
+    print(divided)
+
+print('Chose an operation: a,s,m,d')
+choice = input("enter your choice")
+num1 = int(input("enter a number"))
+num2 = int(input("enter a number"))
+
+if choice == 'a':
+    add(num1,num2)
+if choice == 's':
+    subtract(num1,num2)
+if choice == 'm':
+    multiply(num1,num2)
+if choice == 'd':
+    divide(num1,num2)
+
+ 
